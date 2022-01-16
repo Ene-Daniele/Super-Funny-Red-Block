410badc0bce54f9572e12f0261b95dfb2f101a31
Evryon <ene.daniele@iisgalvanimi.edu.it>
2022-01-17 00:21:20 +0100

    added some todos for tomorrow when im gonna write 2 lines of code every 30 minutes in between helping my classmates fix their collisioons lmao

--- JavaScript/Game Functions/CameraSystem.js
+++ JavaScript/Game Functions/CameraSystem.js
@@ -4,8 +4,15 @@
 
 function cameraPan(box){
 
+    /*
+    TODO Add a camera strength parameter wich is mutliplied by that 30 over there, so like ok so like right
+    TODO so what i was thinking was like << distanceX = ((screen.availWidth / 2 - Player.x) / (30 * strength)) >>
+    TODO and then also << Player.x += distanceX / (8.942854 * strength); >>
+    TODO and then like strength is either 1, or 2, or 3, etc depending on how strong it must be. (1 is the default)
+    */
+
     distanceX = ((screen.availWidth / 2 - Player.x) / 30); // ? sceen.availWidth is AMAZING <3<3<3
-    // * Also i need this variable because it changes overtime, very slightly but it does, so i need the value once every function.
+    // * I need this variable because it changes overtime, very slightly but it does, so i need the value once every function.
 
     box.x += distanceX;
     
@@ -21,15 +28,8 @@ function cameraPan(box){
     }
 
     // Its gonna keep changing the players X value with minuscule values so when it reaches this, i round it up.
+    //TODO Do one for the bigger value aswell, like the right side, this is only for the left side
     if (Player.x == 767.311861849626){ 
         Player.x = 768;
     }
 }   
-
-/*
-? This is full of funny numbers and you might ask, how did i get to those? You wouldnt understand.
-? The amount of sheer intelligence requiered for those mathematical calculations is beyond your comprehension
-? and i truly-
-
-? TRIAL AND ERROR. I KEPT TRYING TIL I GOT CLOSER AND CLOSER AND CLOSER FOR 3 HOURS JESUS CHRIST 
-*/
